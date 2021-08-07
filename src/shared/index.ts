352e36d8f5141e6da6d3bb9e89c9028d6a317fdf
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2021-08-07 11:27:52 -0500

    Add shared components

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,5 +3,5 @@ import Button from "./Button/Button";
 import Flex from "./Flex/Flex";
 import Logo from "./Image/Logo";
 import InputFile from "./InputFile/InputFile";
-
-export { Input, Flex, Button, Logo, InputFile };
+import PulseLoader from "./Loader/PulseLoader";
+export { Input, Flex, Button, Logo, InputFile, PulseLoader };
